a92a6057d064cbff0023c10fb4d918fb203ae24c
mianuj46 <123156023+mianuj46@users.noreply.github.com>
2023-03-10 16:39:38 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,17 @@
+function Header() {
+ return (<>
+    <div className="flex shopping-cart">
+    <div >ReactMeals </div>
+    <div>Cart<sup>{'3'}</sup></div>
+    
+    </div>
+    <div className="meal">
+        <h1 className="cent">Delicious Food,Delivered to you</h1>
+        <p>Choose your favorite meal from our board selection of available meals and enjoy a delicious lunch or dinner at home</p>
+        <p>All our meals are cooked wuth high quality ingredients ,just in time and of course by experiene chefs</p>
+    </div>
+    </>
+ )
+}
+
+export default Header;
