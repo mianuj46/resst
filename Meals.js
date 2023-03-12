818642d1a5a4bc4f9ebbed236f63c9ab06ee7c99
mianuj46 <123156023+mianuj46@users.noreply.github.com>
2023-03-12 08:26:14 +0530

    Add files via upload

---
+++ Meals.js
@@ -0,0 +1,11 @@
+import {Fragment} from 'react'
+import  MealsSummary from './Mealsummary'
+import Availablemeal from './Availablemeal'
+
+const Meals=()=>{
+    return <Fragment>
+        <MealsSummary/>
+        <Availablemeal/>
+    </Fragment>
+}
+export default Meals
